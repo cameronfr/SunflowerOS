fdbef1fb687f337f6bc889dca0c33e613eed01c0
cameronfr <catechboy@hotmail.com>
2022-07-05 21:25:42 -0700

    add YoloX+ViTPose+CrowdPose3D work

---
+++ worldAnalyzer3.py
@@ -0,0 +1,484 @@
+# Yolox-l + vitpose + 3Dcrowdnet
+
+import torch
+import torchvision
+import numpy as np
+import sys
+import mmdet.apis
+import os
+import av
+from PIL import Image
+
+import plotly.express as px
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+plt.rcParams["axes.grid"] = False
+%config InlineBackend.figure_format='svg'
+
+device = torch.cuda.current_device()
+
+# ------------------------------ HELPERS------------------------------
+
+class DictToObject(object):
+	def __init__(self, d):
+		for a, b in d.items():
+			if isinstance(b, (list, tuple)):
+			   setattr(self, a, [obj(x) if isinstance(x, dict) else x for x in b])
+			else:
+			   setattr(self, a, obj(b) if isinstance(b, dict) else b)
+
+def keypointsCOCOToB36M(keypoints):
+	keypoints_new = np.zeros((17, keypoints.shape[1]))
+	# pelvis is in the middle of l_hip and r_hip
+	keypoints_new[0] = (keypoints[11] + keypoints[12]) / 2
+	# thorax is in the middle of l_shoulder and r_shoulder
+	keypoints_new[8] = (keypoints[5] + keypoints[6]) / 2
+	# head is in the middle of l_eye and r_eye
+	keypoints_new[10] = (keypoints[1] + keypoints[2]) / 2
+	# spine is in the middle of thorax and pelvis
+	keypoints_new[7] = (keypoints_new[0] + keypoints_new[8]) / 2
+	# rearrange other keypoints
+	keypoints_new[[1, 2, 3, 4, 5, 6, 9, 11, 12, 13, 14, 15, 16]] = \
+		keypoints[[12, 14, 16, 11, 13, 15, 0, 5, 7, 9, 6, 8, 10]]
+	return keypoints_new
+
+def traverseHierarchy(h, startKey, callback):
+	parentId = startKey
+	if type(h) is dict:
+		children = h.keys()
+		for childKey in children:
+			callback(parentId, childKey)
+			traverseHierarchy(h[childKey], childKey, callback)
+	else:
+		childKey = h
+		callback(parentId, childKey)
+
+# Plots take in B36m format
+def plot3dPose(keypoints3d):
+	# in keypoints, Y(second dim) is up, and Y moves down.
+
+	# keypoint hierarchy starting at root
+	hierarchy = {1: {2: 3}, 4: {5: 6}, 7: {8: {9: 10, 11: {12: 13}, 14: {15: 16}}}}
+	# calls callback(id1, id2) for every bone between joints
+	xs = []
+	ys = []
+	zs = []
+	colors = []
+	def addToPlot(parentId, childId):
+		p1 = keypoints3d[parentId]
+		p2 = keypoints3d[childId]
+		xs.extend([p1[0], p2[0]])
+		ys.extend([p1[2], p2[2]]) #swap y and z, make down->up up->down
+		zs.extend([-p1[1], -p2[1]])
+		boneId = str(parentId) + "-" + str(childId)
+		colors.extend([boneId, boneId])
+
+	traverseHierarchy(hierarchy, 0, addToPlot)
+
+	fig = px.line_3d(x=xs, y=ys, z=zs, color=colors)
+	fig.update_layout(scene_aspectmode='data')
+	fig.show()
+
+def plot2dPose(keypoints2d, overlay=False):
+	hierarchy = {1: {2: 3}, 4: {5: 6}, 7: {8: {9: 10, 11: {12: 13}, 14: {15: 16}}}}
+	# calls callback(id1, id2) for every bone between joints
+	def traverseHierarchy(h, startKey, callback):
+		parentId = startKey
+		if type(h) is dict:
+			children = h.keys()
+			for childKey in children:
+				callback(parentId, childKey)
+				traverseHierarchy(h[childKey], childKey, callback)
+		else:
+			childKey = h
+			callback(parentId, childKey)
+
+	def addToPlot(parentId, childId):
+		p1 = keypoints2d[parentId]
+		p2 = keypoints2d[childId]
+		plt.plot([p1[0], p2[0]], [-p1[1], -p2[1]])
+
+	if not overlay:
+		plt.figure(figsize = (3,7))
+	traverseHierarchy(hierarchy, 0, addToPlot)
+
+# ------------------------------ YOLOX DETECTOR ------------------------------
+
+# in the mmdet Repo
+def loadDetector():
+	os.chdir("/home/cameron/mmdetection/")
+	modelConfig = "configs/yolox/yolox_x_8x8_300e_coco.py"
+	modelCheckpoint = "https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_x_8x8_300e_coco/yolox_x_8x8_300e_coco_20211126_140254-1ef88d67.pth"
+	model = mmdet.apis.init_detector(modelConfig, modelCheckpoint, device) # this is a torch module.
+	sum([x.numel() for x in model.parameters()])
+	return model
+
+videoRaw = av.open("/home/cameron/shibuyaTrim1.mp4")
+# videoRaw.seek(3*10**6)
+videoStream = videoRaw.streams[0]
+videoFrames = videoRaw.decode(videoRaw.streams[0])
+videoArray = np.empty((videoStream.frames, videoStream.height, videoStream.width, 3), dtype=np.uint8) #585MB for 10 seconds of video
+for idx, frame in enumerate(videoRaw.decode(videoRaw.streams[0])):
+	videoArray[idx] = np.array(frame.to_image())
+actualFramecount = int(videoStream.base_rate * videoStream.duration * videoStream.time_base)
+videoArray = videoArray[:actualFramecount]
+
+# frames = np.array(Image.open("/home/cameron/Crowdnet3D/demo/input/images/100023.jpg"))[np.newaxis, ...]
+frames = videoArray[:]#[0:100]
+
+detectorModule = loadDetector()
+detectorOut = []
+for frame in frames:
+	detectorOut.append(mmdet.apis.inference_detector(detectorModule, frame[:, :, ::-1]))
+
+# resVis = detectorModule.show_result(frames[61], detectorOut[61], score_thr=0.3, bbox_color=None, text_color=(200, 200, 200), mask_color=None)
+# plt.figure(figsize = (10,10))
+# plt.imshow(resVis)
+
+# ------------------------------ ViTPose ------------------------------
+# uses frames and detectorOut from previous step
+
+sys.path.insert(0, "/home/cameron/vitpose/") # load the modified mmpose from the vitpose repo. Modifiy mmpose/mmpose/__init__.py so that "1.5.0" max is replaced w/ current install
+import mmpose.apis
+# from mmpose.apis import inference_top_down_pose_model, vis_pose_result, get_track_id # its messy / not good structure, resist urge to clean up
+import copy
+import tqdm
+
+def loadPose2D():
+	os.chdir("/home/cameron/")
+	modelConfig = "vitpose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_large_coco_256x192.py"
+	modelCheckpoint = "vitpose-l-multi-coco.pth" #download from onedrive
+	model = mmpose.apis.init_pose_model(modelConfig, modelCheckpoint, device) # this is a torch module.
+	sum([x.numel() for x in model.parameters()])
+	return model
+poseModel = loadPose2D()
+len(detectorOut)
+
+posesByFrame = []
+nextTrackId = 0
+lastPose2dOutWithTrackId = []
+for frameIdx, detectionsInFrame in tqdm.tqdm(enumerate(detectorOut)):
+	people = detectionsInFrame[0]
+	confidences = people[:, 4]
+	peopleBboxes = [{"bbox": x} for x in people[confidences > 0.1]]
+	pose2dOut, _ = inference_top_down_pose_model(poseModel, frames[frameIdx][:, :, ::-1], person_results=peopleBboxes, bbox_thr=0.0, format="xyxy")
+
+	# pose2dOut is list of {"keypoints" ..., "bbox": ...}, and pose2dOutWithTrackId adds a "track id" to each dict.
+	pose2dOutWithTrackId, nextTrackId = get_track_id(pose2dOut, copy.deepcopy(lastPose2dOutWithTrackId), nextTrackId, use_oks=True, tracking_thr=0.3) # code deletes objs in last pose...
+	posesByFrame.append(pose2dOutWithTrackId)
+	lastPose2dOutWithTrackId = pose2dOutWithTrackId
+
+	# vis = vis_pose_result(poseModel, frames[frameIdx], pose2dOut, kpt_score_thr=0.0, radius=4, thickness=2)
+	# plt.figure(figsize = (10,10))
+	# plt.imshow(vis)
+	# break
+
+# vis = vis_pose_result(poseModel, frames[61], posesByFrame[61], kpt_score_thr=0.1, radius=4, thickness=2)
+# plt.figure(figsize = (10,10))
+# plt.imshow(vis)
+
+posesByTrackId = {}
+for frameIdx, poses in enumerate(posesByFrame):
+	for pose in poses:
+		track_id = pose["track_id"]
+		if track_id not in posesByTrackId:
+			posesByTrackId[track_id] = []
+		else:
+			if posesByTrackId[track_id][-1]["frameIdx"] != frameIdx - 1:
+				raise "track is non contiguous, not expected"
+		pose["frameIdx"] = frameIdx
+		posesByTrackId[track_id].append(pose)
+
+# ------------------------------ CrowdPose3d ------------------------------
+# uses posesByTrackId and frames
+
+vis = vis_pose_result(poseModel, frames[posesByTrackId[0][0]["frameIdx"]], [posesByTrackId[0][0]], kpt_score_thr=0.0, radius=4, thickness=2)
+plt.figure(figsize = (10,10))
+plt.imshow(vis)
+plot2dPose(keypointsCOCOToB36M(posesByTrackId[0][0]["keypoints"]))
+import json
+pose2d_result_path = '/home/cameron/Crowdnet3D/demo/input/2d_pose_result.json'
+with open(pose2d_result_path) as f:
+	pose2d_result = json.load(f)
+poses = np.array(pose2d_result["100023.jpg"])
+poses.shape
+plot2dPose(keypointsCOCOToB36M(poses[0]))
+
+os.chdir("/home/cameron/Crowdnet3D/demo")
+import sys
+import os
+import os.path as osp
+import argparse
+import numpy as np
+import cv2
+import colorsys
+import json
+import random
+import torch
+import torchvision.transforms as transforms
+from torch.nn.parallel.data_parallel import DataParallel
+import torch.backends.cudnn as cudnn
+import matplotlib.pyplot as plt
+
+sys.path.insert(0, osp.join('..', 'main'))
+sys.path.insert(0, osp.join('..', 'data'))
+sys.path.insert(0, osp.join('..', 'common'))
+from config import cfg
+from model import get_model
+from utils.preprocessing import process_bbox, generate_patch_image, get_bbox
+from utils.transforms import pixel2cam, cam2pixel, transform_joint_to_other_db
+os.environ["MESA_GL_VERSION_OVERRIDE"] = "4.1"
+from utils.vis import vis_mesh, save_obj, render_mesh, vis_coco_skeleton
+sys.path.insert(0, cfg.smpl_path)
+from utils.smpl import SMPL
+
+def add_pelvis(joint_coord, joints_name):
+	lhip_idx = joints_name.index('L_Hip')
+	rhip_idx = joints_name.index('R_Hip')
+	pelvis = (joint_coord[lhip_idx, :] + joint_coord[rhip_idx, :]) * 0.5
+	pelvis[2] = joint_coord[lhip_idx, 2] * joint_coord[rhip_idx, 2]  # confidence for openpose
+	pelvis = pelvis.reshape(1, 3)
+
+	joint_coord = np.concatenate((joint_coord, pelvis))
+
+	return joint_coord
+def add_neck(joint_coord, joints_name):
+	lshoulder_idx = joints_name.index('L_Shoulder')
+	rshoulder_idx = joints_name.index('R_Shoulder')
+	neck = (joint_coord[lshoulder_idx, :] + joint_coord[rshoulder_idx, :]) * 0.5
+	neck[2] = joint_coord[lshoulder_idx, 2] * joint_coord[rshoulder_idx, 2]
+	neck = neck.reshape(1,3)
+
+	joint_coord = np.concatenate((joint_coord, neck))
+
+	return joint_coord
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--gpu', type=str, dest='gpu_ids')
+	parser.add_argument('--model_path', type=str, default='demo_checkpoint.pth.tar')
+	parser.add_argument('--img_idx', type=str, default='101570')
+
+	args = parser.parse_args(["--gpu", "0"])
+
+	# test gpus
+	if not args.gpu_ids:
+		assert 0, print("Please set proper gpu ids")
+
+	if '-' in args.gpu_ids:
+		gpus = args.gpu_ids.split('-')
+		gpus[0] = int(gpus[0])
+		gpus[1] = int(gpus[1]) + 1
+		args.gpu_ids = ','.join(map(lambda x: str(x), list(range(*gpus))))
+
+	return args
+args = parse_args()
+cfg.set_args(args.gpu_ids, is_test=True)
+cfg.render = True
+cudnn.benchmark = True
+
+# SMPL joint set
+joint_num = 30  # original: 24. manually add nose, L/R eye, L/R ear, head top
+joints_name = (
+'Pelvis', 'L_Hip', 'R_Hip', 'Torso', 'L_Knee', 'R_Knee', 'Spine', 'L_Ankle', 'R_Ankle', 'Chest', 'L_Toe', 'R_Toe',
+'Neck', 'L_Thorax', 'R_Thorax',
+'Head', 'L_Shoulder', 'R_Shoulder', 'L_Elbow', 'R_Elbow', 'L_Wrist', 'R_Wrist', 'L_Hand', 'R_Hand', 'Nose', 'L_Eye',
+'R_Eye', 'L_Ear', 'R_Ear', 'Head_top')
+flip_pairs = (
+(1, 2), (4, 5), (7, 8), (10, 11), (13, 14), (16, 17), (18, 19), (20, 21), (22, 23), (25, 26), (27, 28))
+skeleton = (
+(0, 1), (1, 4), (4, 7), (7, 10), (0, 2), (2, 5), (5, 8), (8, 11), (0, 3), (3, 6), (6, 9), (9, 14), (14, 17), (17, 19),
+(19, 21), (21, 23), (9, 13), (13, 16), (16, 18), (18, 20), (20, 22), (9, 12), (12, 24), (24, 15), (24, 25), (24, 26),
+(25, 27), (26, 28), (24, 29))
+
+# SMPl mesh
+vertex_num = 6890
+smpl = SMPL()
+face = smpl.face
+
+# other joint set
+coco_joints_name = ('Nose', 'L_Eye', 'R_Eye', 'L_Ear', 'R_Ear', 'L_Shoulder', 'R_Shoulder', 'L_Elbow', 'R_Elbow', 'L_Wrist', 'R_Wrist', 'L_Hip', 'R_Hip', 'L_Knee', 'R_Knee', 'L_Ankle', 'R_Ankle', 'Pelvis', 'Neck')
+coco_skeleton = (
+(1, 2), (0, 1), (0, 2), (2, 4), (1, 3), (6, 8), (8, 10), (5, 7), (7, 9), (12, 14), (14, 16), (11, 13), (13, 15), (5, 6),
+(11, 17), (12,17), (17,18))
+
+vis_joints_name = ('Nose', 'L_Eye', 'R_Eye', 'L_Ear', 'R_Ear', 'L_Shoulder', 'R_Shoulder', 'L_Elbow', 'R_Elbow', 'L_Wrist', 'R_Wrist', 'L_Hip', 'R_Hip', 'L_Knee', 'R_Knee', 'L_Ankle', 'R_Ankle', 'Thorax', 'Pelvis')
+vis_skeleton = ((0, 1), (0, 2), (2, 4), (1, 3), (5, 7), (7, 9), (12, 14), (14, 16), (11, 13), (13, 15), (5, 17), (6, 17), (11, 18), (12, 18), (17, 18), (17, 0), (6, 8), (8, 10),)
+
+# snapshot load
+model_path = args.model_path
+assert osp.exists(model_path), 'Cannot find model at ' + model_path
+print('Load checkpoint from {}'.format(model_path))
+model = get_model(vertex_num, joint_num, 'test')
+
+model = DataParallel(model).cuda()
+ckpt = torch.load(model_path)
+model.load_state_dict(ckpt['network'], strict=False)
+model.eval()
+
+# prepare input image
+transform = transforms.ToTensor()
+pose2d_result_path = './input/2d_pose_result.json'
+with open(pose2d_result_path) as f:
+	pose2d_result = json.load(f)
+
+# os.getcwd()
+# img_dir = './input/images'
+# for img_name in sorted(pose2d_result.keys()):
+for frameIdx, frame in tqdm.tqdm(enumerate(frames[:])):
+	frameIdx = 61
+	# img_path = osp.join(img_dir, img_name)
+	# image.shape
+	# original_img.shape
+	# original_img = cv2.imread(img_path)
+
+	# coco_joint_list = pose2d_result[img_name]
+	# image = np.array(Image.open(img_path))
+	img_name = f"""frame{frameIdx}.jpg"""
+	img_path = img_name
+	coco_joint_list = [pose["keypoints"].tolist() for pose in posesByFrame[frameIdx]]
+	image = frames[frameIdx]
+	original_img = image[:, :, ::-1]
+	input = original_img.copy()
+	input2 = original_img.copy()
+	# plt.imshow(input)
+	original_img_height, original_img_width = original_img.shape[:2]
+	# np.array(pose2d_result[img_name]).shape
+	# plt.hist(np.array(pose2d_result[img_name])[:, :, 4].ravel())
+
+	drawn_joints = []
+	c = coco_joint_list
+	# manually assign the order of output meshes
+	# coco_joint_list = [c[2], c[0], c[1], c[4], c[3]]
+
+	for idx in range(len(coco_joint_list)):
+		""" 2D pose input setting & hard-coding for filtering """
+		pose_thr = 0.1
+		coco_joint_img = np.asarray(coco_joint_list[idx])[:, :3]
+		coco_joint_img = add_pelvis(coco_joint_img, coco_joints_name)
+		coco_joint_img = add_neck(coco_joint_img, coco_joints_name)
+		coco_joint_valid = (coco_joint_img[:, 2].copy().reshape(-1, 1) > pose_thr).astype(np.float32)
+
+		# filter inaccurate inputs
+		det_score = sum(coco_joint_img[:, 2])
+		if det_score < 1.0:
+			continue
+		if len(coco_joint_img[:, 2:].nonzero()[0]) < 1:
+			continue
+		# filter the same targets
+		tmp_joint_img = coco_joint_img.copy()
+		continue_check = False
+		for ddx in range(len(drawn_joints)):
+			drawn_joint_img = drawn_joints[ddx]
+			drawn_joint_val = (drawn_joint_img[:, 2].copy().reshape(-1, 1) > pose_thr).astype(np.float32)
+			diff = np.abs(tmp_joint_img[:, :2] - drawn_joint_img[:, :2]) * coco_joint_valid * drawn_joint_val
+			diff = diff[diff != 0]
+			if diff.size == 0:
+				continue_check = True
+			elif diff.mean() < 20:
+				continue_check = True
+		if continue_check:
+			continue
+		drawn_joints.append(tmp_joint_img)
+
+		""" Prepare model input """
+		# prepare bbox
+		bbox = get_bbox(coco_joint_img, coco_joint_valid[:, 0]) # xmin, ymin, width, height
+		bbox = process_bbox(bbox, original_img_width, original_img_height)
+		if bbox is None:
+			continue
+		img, img2bb_trans, bb2img_trans = generate_patch_image(input2[:,:,::-1], bbox, 1.0, 0.0, False, cfg.input_img_shape)
+		img = transform(img.astype(np.float32))/255
+		img = img.cuda()[None,:,:,:]
+
+		coco_joint_img_xy1 = np.concatenate((coco_joint_img[:, :2], np.ones_like(coco_joint_img[:, :1])), 1)
+		coco_joint_img[:, :2] = np.dot(img2bb_trans, coco_joint_img_xy1.transpose(1, 0)).transpose(1, 0)
+		coco_joint_img[:, 0] = coco_joint_img[:, 0] / cfg.input_img_shape[1] * cfg.output_hm_shape[2]
+		coco_joint_img[:, 1] = coco_joint_img[:, 1] / cfg.input_img_shape[0] * cfg.output_hm_shape[1]
+
+		coco_joint_img = transform_joint_to_other_db(coco_joint_img, coco_joints_name, joints_name)
+		coco_joint_valid = transform_joint_to_other_db(coco_joint_valid, coco_joints_name, joints_name)
+		coco_joint_valid[coco_joint_img[:, 2] <= pose_thr] = 0
+
+		# check truncation
+		coco_joint_trunc = coco_joint_valid * ((coco_joint_img[:, 0] >= 0) * (coco_joint_img[:, 0] < cfg.output_hm_shape[2]) * (coco_joint_img[:, 1] >= 0) * (coco_joint_img[:, 1] < cfg.output_hm_shape[1])).reshape(
+			-1, 1).astype(np.float32)
+		coco_joint_img, coco_joint_trunc, bbox = torch.from_numpy(coco_joint_img).cuda()[None, :, :], torch.from_numpy(coco_joint_trunc).cuda()[None, :, :], torch.from_numpy(bbox).cuda()[None, :]
+
+		""" Model forward """
+		inputs = {'img': img, 'joints': coco_joint_img, 'joints_mask': coco_joint_trunc}
+		targets = {}
+		meta_info = {'bbox': bbox}
+		with torch.no_grad():
+			out = model(inputs, targets, meta_info, 'test')
+
+		# draw output mesh
+		mesh_cam_render = out['mesh_cam_render'][0].cpu().numpy()
+		bbox = out['bbox'][0].cpu().numpy()
+		princpt = (bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2)
+		# original_img = vis_bbox(original_img, bbox, alpha=1)  # for debug
+
+		# generate random color
+		color = colorsys.hsv_to_rgb(np.random.rand(), 0.5, 1.0)
+		original_img = render_mesh_2(original_img, mesh_cam_render, face, {'focal': cfg.focal, 'princpt': princpt}, color=color)
+		# plt.imshow(original_img[:, :, ::-1])
+		# plt.show()
+
+		# Save output mesh
+		# output_dir = 'output_custom'
+		file_name = f'{output_dir}/{img_path.split("/")[-1][:-4]}_{idx}.jpg'
+	print("file name: ", file_name)
+		# save_obj(mesh_cam_render, face, file_name=f'{output_dir}/{img_path.split("/")[-1][:-4]}_{idx}.obj')
+	cv2.imwrite(file_name, original_img)
+
+		# Draw input 2d pose
+		# tmp_joint_img[-1], tmp_joint_img[-2] = tmp_joint_img[-2].copy(), tmp_joint_img[-1].copy()
+		# input = vis_coco_skeleton(input, tmp_joint_img.T, vis_skeleton)
+		# cv2.imwrite(file_name[:-4] + '_2dpose.jpg', input)
+
+import os
+import cv2
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import trimesh
+os.environ['PYOPENGL_PLATFORM'] = 'egl'
+os.environ["MESA_GL_VERSION_OVERRIDE"] = "4.1"
+import pyrender
+
+def render_mesh_2(img, mesh, face, cam_param, color=(1.0, 1.0, 0.9, 1.0)):
+    # mesh
+    mesh = trimesh.Trimesh(mesh, face)
+    rot = trimesh.transformations.rotation_matrix(np.radians(180), [1, 0, 0])
+    mesh.apply_transform(rot)
+    material = pyrender.MetallicRoughnessMaterial(metallicFactor=0.0, alphaMode='OPAQUE', baseColorFactor=color)
+    mesh = pyrender.Mesh.from_trimesh(mesh, material=material, smooth=False)
+    scene = pyrender.Scene(bg_color=[0.0, 0.0, 0.0, 0.0], ambient_light=(0.3, 0.3, 0.3))
+    scene.add(mesh, 'mesh')
+
+    focal, princpt = cam_param['focal'], cam_param['princpt']
+    camera = pyrender.IntrinsicsCamera(fx=focal[0], fy=focal[1], cx=princpt[0], cy=princpt[1], zfar=1000.0, znear=0.05)
+    scene.add(camera)
+
+    # renderer
+    renderer = pyrender.OffscreenRenderer(viewport_width=img.shape[1], viewport_height=img.shape[0], point_size=1.0)
+
+    # light
+    light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=0.8)
+    light_pose = np.eye(4)
+    light_pose[:3, 3] = np.array([0, -1, 1])
+    scene.add(light, pose=light_pose)
+    light_pose[:3, 3] = np.array([0, 1, 1])
+    scene.add(light, pose=light_pose)
+    light_pose[:3, 3] = np.array([1, 1, 2])
+    scene.add(light, pose=light_pose)
+
+    # render
+    rgb, depth = renderer.render(scene, flags=pyrender.RenderFlags.RGBA)
+    rgb = rgb[:, :, :3].astype(np.float32)
+    valid_mask = (depth > 0)[:, :, None]
+
+    # save to image
+    img = rgb * valid_mask + img * (1 - valid_mask)
+    return img.astype(np.uint8)
